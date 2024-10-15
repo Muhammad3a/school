fd54f9f2ecd7b35e3738651af4fa8e88a37898e8
Muhammad3a <118835652+Muhammad3a@users.noreply.github.com>
2024-10-15 12:23:04 +0700

    Update 2024_10_13_034616_create_students_table.php

--- database/migrations/2024_10_13_034616_create_students_table.php
+++ database/migrations/2024_10_13_034616_create_students_table.php
@@ -17,7 +17,6 @@ return new class extends Migration
             $table->string('name');
             $table->enum('gender', ['Male', 'Female'])->default('Male');
             $table->date('birthday')->nullable();
-            $table->enum('religion', ['Islam', 'Katolik', 'Protestan', 'Hindu', 'Buddha', 'Khonghucu'])->default('Islam');
             $table->string('contact')->nullable();
             $table->string('profile')->nullable();
 
