5d246cd5d8cd36436cf9574b2aec2235df3ad9b8
Muhammad3a <118835652+Muhammad3a@users.noreply.github.com>
2024-10-20 17:56:42 +0700

    nilai di panel baru

--- app/Models/Classroom.php
+++ app/Models/Classroom.php
@@ -4,6 +4,8 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Classroom extends Model
 {
@@ -13,5 +15,10 @@ class Classroom extends Model
     public function subjects()
     {
         return $this->belongsToMany(Subject::class)->withPivot('description');
+    }
+
+    public function students(): BelongsToMany
+    {
+        return $this->belongsToMany(Student::class, 'student_has_classes', 'classrooms_id', 'students_id');
     }
 }
