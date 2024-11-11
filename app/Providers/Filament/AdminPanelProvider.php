fb5a54ea4be980c958836c7bf2aece711afa74c0
MYusupNH <127910886+MYusupNH@users.noreply.github.com>
2024-11-11 11:46:29 +0700

    update

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -144,13 +144,13 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->colors([
-                // 'danger' => Color::Rose,
-                // 'gray' => Color::Gray,
-                // 'info' => Color::Blue,
-                // 'primary' => Color::Indigo,
-                // 'success' => Color::Emerald,
-                // 'warning' => Color::Orange,
-                'primary' => Color::Amber,
+                'danger' => Color::Rose,
+                'gray' => Color::Gray,
+                'info' => Color::Blue,
+                'primary' => Color::Indigo,
+                'success' => Color::Emerald,
+                'warning' => Color::Orange,
+                // 'primary' => Color::Amber,
             ])
             ->discoverResources(in: app_path('Filament/Resources'), for: 'App\\Filament\\Resources')
             ->discoverPages(in: app_path('Filament/Pages'), for: 'App\\Filament\\Pages')
